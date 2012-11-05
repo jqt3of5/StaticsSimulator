e0037e2b7b0adececbc8f3f3175ecea2c05c722f
John Todd <jqt3of5@gmail.com>
2012-11-04 22:48:24 -0700

    First commit. W00T

---
+++ desktop/Statics_Simulator_Linux/Main.cs
@@ -0,0 +1,16 @@
+using System;
+using Gtk;
+
+namespace Statics_Simulator_Linux
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Application.Init ();
+			MainWindow win = new MainWindow ();
+			win.Show ();
+			Application.Run ();
+		}
+	}
+}
