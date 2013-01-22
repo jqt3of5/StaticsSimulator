7b3f57c1bfc17f6a0bd4a3a6963e769b635ee4c0
John Todd <jqt3of5@gmail.com>
2013-01-21 19:48:12 -0700

    see previous commid

--- UI/UI/MainWindow.cs
+++ UI/UI/MainWindow.cs
@@ -1,16 +1,24 @@
 using System;
 using Gtk;
-
+using UI;
 public partial class MainWindow: Gtk.Window
 {	
 	public MainWindow (): base (Gtk.WindowType.Toplevel)
 	{
 		Build ();
+		//add the special drawing window here
+		DrawingBoxAlignment.Add(new DrawingWidget());
+		//need to populate the toolbar with tools. 
+		//
+		//
+		//
 	}
 	
+
 	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
 	{
 		Application.Quit ();
 		a.RetVal = true;
 	}
+	
 }
